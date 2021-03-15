811c39982bbca69e279eb519480479a70bb95ca0
Ihar Semashka <6399815@mail.ru>
2021-03-16 01:22:53 +0300

    init: start finish task reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+        n *= -1;
+    }
+    n +="";
+    let reverse ="";
+    for(let i = n.length - 1; i >= 0; i--)
+    {
+        reverse += n[i];
+    }
+    return parseInt(reverse)
 }
